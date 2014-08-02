ad0acd50c5c8045979313f710f6801a9f1575b6e
Kai Davenport <kaiyadavenport@gmail.com>
2014-08-02 14:56:08 -0700

    trig setup working sweet

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var fs = require('fs')
 var path = require('path')
 var trig = require('trig')
 var command = process.argv[2] || 'help'
-
 var commandPath = path.join(__dirname, 'commands', command + '.js')
 
 if(fs.existsSync(commandPath)){
@@ -10,6 +9,11 @@ if(fs.existsSync(commandPath)){
 }
 else{
 	var args = process.argv.splice(3)
+	var files = [path.join(__dirname, 'triggers', 'default')]
+	if(process.env.VIKING_TRIGGERS && fs.existsSync(process.env.VIKING_TRIGGERS)){
+		files.push(process.env.VIKING_TRIGGERS)
+	}
+	var command = trig.plan(files, command, args)
 	console.log('-------------------------------------------');
-	console.dir(args)
+	console.dir(command)
 }
--- triggers/ps.js
+++ triggers/ps.js
@@ -9,12 +9,4 @@ var args = require('minimist')(process.argv, {
 	}
 })
 
-var topic = args._[2]
-var baseFolder = path.join(__dirname, path.normalize('../docs/help'))
-
-if(!fs.existsSync(path.join(baseFolder, topic + '.txt'))){
-	topic = 'viking'
-}
-
-var content = fs.readFileSync(path.join(baseFolder, topic + '.txt'), 'utf8')
-console.log(content)
+console.log('yo here be ps')
