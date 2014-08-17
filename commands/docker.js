f17c00c8f5d90b0f5da0c5741f5fca53654f7fc4
Kai Davenport <kaiyadavenport@gmail.com>
2014-08-17 12:54:20 -0700

    split the docker run command by the image to not get confused by --volume or --link arguments to the docker command

--- commands/docker.js
+++ commands/docker.js
@@ -1,10 +1,19 @@
-var args = require('minimist')(process.argv)
-var dockerargv = process.argv.slice(2)
+var strippedArgs = process.argv.slice(2)
+var args = require('minimist')(strippedArgs)
 
-console.dir(process.argv)
-console.dir(dockerargv)
+// check for volumes and links
+if(args._[1]=='run'){
 
+	// split the command into docker args and command args
+	var image = args._[2]
 
-if(args._[2]=='run'){
+	var hit = false
+	var dockerargs = strippedArgs.filter(function(arg){
+		if(arg==image){
+			hit = true
+		}
+		return !hit
+	})
 
+	console.log(dockerargs)
 }
